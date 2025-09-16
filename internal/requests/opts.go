58287d29350d9649203537c707bf1dde4b1b4118
Juan Fernández <juan@bitomia.com>
2025-09-16 07:49:31 +0200

    fix: missed file

---
+++ internal/requests/opts.go
@@ -0,0 +1,27 @@
+package requests
+
+type CpuCFS struct {
+	CpuQuota  int64  `json:"quota"`
+	CpuPeriod uint64 `json:"period"`
+}
+
+type Quotas struct {
+	VolumeSize *string `json:"volume_size,omitempty"`
+	MemLimit   *uint64 `json:"mem_limit,omitempty"` // Megabytes
+	CpuCFS     *CpuCFS `json:"cpu_cfs,omitempty"`
+	CpuShares  *uint64 `json:"cpu_shares,omitempty"`
+}
+
+type MountVolume struct {
+	Volume string `json:"volume"`
+	Target string `json:"target"`
+}
+
+type CreateContainerOpts struct {
+	Image            string      `json:"image"`
+	Owner            string      `json:"owner"`
+	VolumeMountPoint string      `json:"volume_mount_point,omitempty"`
+	MountVolume      MountVolume `json:"mount_volume,omitempty"`
+	Env              []string    `json:"env,omitempty"`
+	Quotas           Quotas      `json:"quotas,omitempty"`
+}
