56a46f7460afcfedf9af1f1b0e51075716b72b0e
Michael Bignet <michael@bitomia.com>
2025-11-28 23:50:25 +0100

    feat(lib): getNodeStatus now returns a typed struct

--- lib/daemon/daemon.go
+++ lib/daemon/daemon.go
@@ -2,6 +2,33 @@ package main
 
 /*
 #include <stdlib.h>
+#include <string.h>
+
+typedef struct {
+    char* container_id;
+    double cpu_usage;
+    double cpu_system;
+    double cpu_user;
+    double memory_usage;
+    double memory_limit;
+    double memory_percent;
+} ContainerStateResponse;
+
+typedef struct {
+    int num_cpu;
+    unsigned long long user_cpu;
+    unsigned long long idle_cpu;
+    unsigned long long system_cpu;
+    unsigned long long total_cpu;
+    double usage_cpu_percent;
+    unsigned long long total_mem;
+    unsigned long long used_mem;
+    unsigned long long free_mem;
+    double free_mem_percent;
+    unsigned long long free_storage;
+    ContainerStateResponse* containers;
+    int containers_count;
+} NodeStateResponse;
 */
 import "C"
 
@@ -63,12 +90,77 @@ func GetHealthStatus() *C.char {
  * @return JSON response with node status
  */
 //export GetNodeStatus
-func GetNodeStatus() *C.char {
-	status, err := api.GetNodeState()
-	if err != nil {
-		return C.CString(api.ResponseToJSON(false, nil, err.Error()))
+func GetNodeStatus() *C.NodeStateResponse {
+	state, err := api.GetNodeState()
+	if err != nil || state == nil {
+		return nil
 	}
-	return C.CString(api.ResponseToJSON(true, status, ""))
+
+	// Allocate NodeStateResponse in C heap
+	resp := (*C.NodeStateResponse)(C.malloc(C.size_t(C.sizeof_NodeStateResponse)))
+	if resp == nil {
+		return nil
+	}
+	resp.num_cpu = C.int(state.NumCPU)
+	resp.user_cpu = C.ulonglong(state.UserCPU)
+	resp.idle_cpu = C.ulonglong(state.IdleCPU)
+	resp.system_cpu = C.ulonglong(state.SystemCPU)
+	resp.total_cpu = C.ulonglong(state.TotalCPU)
+	resp.usage_cpu_percent = C.double(state.UsageCPUPercent)
+	resp.total_mem = C.ulonglong(state.TotalMem)
+	resp.used_mem = C.ulonglong(state.UsedMem)
+	resp.free_mem = C.ulonglong(state.FreeMem)
+	resp.free_mem_percent = C.double(state.FreeMemPercent)
+	resp.free_storage = C.ulonglong(state.FreeStorage)
+	resp.containers = nil
+	resp.containers_count = C.int(0)
+
+	// Allocate containers array if present
+	if len(state.Containers) > 0 {
+		count := C.int(len(state.Containers))
+		bytes := C.size_t(C.sizeof_ContainerStateResponse) * C.size_t(count)
+
+		cArr := (*C.ContainerStateResponse)(C.malloc(bytes))
+		if cArr == nil {
+			return resp // no containers
+		}
+
+		// Turn cArr into a Go slice view
+		containers := (*[1 << 30]C.ContainerStateResponse)(
+			unsafe.Pointer(cArr),
+		)[:count:count]
+
+		for i, c := range state.Containers {
+			containers[i].container_id = C.CString(c.ContainerID)
+			containers[i].cpu_usage = C.double(c.CPUUsage)
+			containers[i].cpu_system = C.double(c.CPUSystem)
+			containers[i].cpu_user = C.double(c.CPUUser)
+			containers[i].memory_usage = C.double(c.MemoryUsage)
+			containers[i].memory_limit = C.double(c.MemoryLimit)
+			containers[i].memory_percent = C.double(c.MemoryPercent)
+		}
+
+		resp.containers = cArr
+		resp.containers_count = count
+	}
+	return resp
+}
+
+//export FreeNodeStateResponse
+func FreeNodeStateResponse(resp *C.NodeStateResponse) {
+	if resp == nil {
+		return
+	}
+	if resp.containers != nil && resp.containers_count > 0 {
+		for i := 0; i < int(resp.containers_count); i++ {
+			entry := (*C.ContainerStateResponse)(unsafe.Pointer(uintptr(unsafe.Pointer(resp.containers)) + uintptr(i)*uintptr(C.sizeof_ContainerStateResponse)))
+			if entry.container_id != nil {
+				C.free(unsafe.Pointer(entry.container_id))
+			}
+		}
+		C.free(unsafe.Pointer(resp.containers))
+	}
+	C.free(unsafe.Pointer(resp))
 }
 
 /**
