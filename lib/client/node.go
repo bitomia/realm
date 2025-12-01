3c1a9869ca544256b92501183ce5888c20922caf
Michael Bignet <michael@bitomia.com>
2025-12-01 12:24:56 +0100

    fix: client now only uses C allocated memory, no longer returns GO pointers, should fix potential memory corruption/leak

--- lib/client/node.go
+++ lib/client/node.go
@@ -40,6 +40,7 @@ import (
 	"unsafe"
 
 	clientPkg "github.com/bitomia/realm/cmd/client"
+	"github.com/bitomia/realm/internal/dto"
 )
 
 //export GetNodesState
@@ -47,35 +48,83 @@ func GetNodesState() *C.NodesStateResponse {
 	client := clientPkg.NewClient()
 	nodes := clientPkg.GetNodes()
 
-	// Collect all node states
-	nodeStates := make([]C.NodeStateResponse, 0, len(nodes))
-
+	// First collect only successful node states
+	// Reason we sanitize here is to allocate the exact amount of C memory needed
+	// Because it looks like clientPkg.GetNodes() return all nodes from the config
+	// even if they are unhealthy or offline.
+	// TODO? log unreachable nodes?
+	statuses := make([]*dto.NodeStateResponse, 0, len(nodes))
 	for _, node := range nodes {
 		status, err := client.GetNodeState(node.Url)
-		if err != nil {
+		if err != nil || status == nil {
 			continue
 		}
+		statuses = append(statuses, status)
+	}
 
-		// Convert containers to C array
+	if len(statuses) == 0 {
+		return nil
+	}
+
+	// Allocate response in C heap
+	resp := (*C.NodesStateResponse)(C.malloc(C.size_t(C.sizeof_NodesStateResponse)))
+	if resp == nil {
+		return nil
+	}
+
+	count := len(statuses)
+	resp.nodes_count = C.int(count)
+
+	// Allocate C array of NodeStateResponse
+	bytes := C.size_t(C.sizeof_NodeStateResponse) * C.size_t(count)
+	resp.nodes = (*C.NodeStateResponse)(C.malloc(bytes))
+	if resp.nodes == nil {
+		C.free(unsafe.Pointer(resp))
+		return nil
+	}
+
+	// Create a Go slice view over the C-allocated array using
+	// the "big array trick". Creates a fake array with a very large size
+	// and then slices it to the actual size we need.
+	nodesSlice := (*[1 << 30]C.NodeStateResponse)(
+		unsafe.Pointer(resp.nodes),
+	)[:count:count]
+
+	for i, status := range statuses {
 		var cContainers *C.ContainerStateResponse
+		var containersCount C.int
+
 		if len(status.Containers) > 0 {
-			containers := make([]C.ContainerStateResponse, len(status.Containers))
-			for i, container := range status.Containers {
-				containers[i] = C.ContainerStateResponse{
-					container_id:   C.CString(container.ContainerID),
-					cpu_usage:      C.double(container.CPUUsage),
-					cpu_system:     C.double(container.CPUSystem),
-					cpu_user:       C.double(container.CPUUser),
-					memory_usage:   C.double(container.MemoryUsage),
-					memory_limit:   C.double(container.MemoryLimit),
-					memory_percent: C.double(container.MemoryPercent),
+			containersCount = C.int(len(status.Containers))
+			// convert to Go int, not same as C.int on all platforms
+			// ie: 32-bit vs 64-bit, realistically should never overflow in this case
+			count_GOint := int(containersCount)
+
+			cBytes := C.size_t(C.sizeof_ContainerStateResponse) * C.size_t(containersCount)
+			cArr := (*C.ContainerStateResponse)(C.malloc(cBytes))
+			if cArr != nil {
+				// Same big fake array trick for containers
+				containersSlice := (*[1 << 30]C.ContainerStateResponse)(
+					unsafe.Pointer(cArr),
+				)[:count_GOint:count_GOint]
+
+				for j, c := range status.Containers {
+					containersSlice[j].container_id = C.CString(c.ContainerID)
+					containersSlice[j].cpu_usage = C.double(c.CPUUsage)
+					containersSlice[j].cpu_system = C.double(c.CPUSystem)
+					containersSlice[j].cpu_user = C.double(c.CPUUser)
+					containersSlice[j].memory_usage = C.double(c.MemoryUsage)
+					containersSlice[j].memory_limit = C.double(c.MemoryLimit)
+					containersSlice[j].memory_percent = C.double(c.MemoryPercent)
 				}
+
+				cContainers = cArr
+			} else {
+				containersCount = 0
 			}
-			cContainers = &containers[0]
 		}
 
-		// Create the node response
-		nodeState := C.NodeStateResponse{
+		nodesSlice[i] = C.NodeStateResponse{
 			num_cpu:           C.int(status.NumCPU),
 			user_cpu:          C.ulonglong(status.UserCPU),
 			idle_cpu:          C.ulonglong(status.IdleCPU),
@@ -88,23 +137,11 @@ func GetNodesState() *C.NodesStateResponse {
 			free_mem_percent:  C.double(status.FreeMemPercent),
 			free_storage:      C.ulonglong(status.FreeStorage),
 			containers:        cContainers,
-			containers_count:  C.int(len(status.Containers)),
+			containers_count:  containersCount,
 		}
-
-		nodeStates = append(nodeStates, nodeState)
 	}
 
-	if len(nodeStates) == 0 {
-		return nil
-	}
-
-	// Create the response with all nodes
-	response := &C.NodesStateResponse{
-		nodes:       &nodeStates[0],
-		nodes_count: C.int(len(nodeStates)),
-	}
-
-	return response
+	return resp
 }
 
 //export FreeNodesStateResponse
@@ -113,24 +150,34 @@ func FreeNodesStateResponse(response *C.NodesStateResponse) {
 		return
 	}
 
-	// Free each node's data
-	if response.nodes != nil {
-		nodes := (*[1 << 30]C.NodeStateResponse)(unsafe.Pointer(response.nodes))[:response.nodes_count:response.nodes_count]
+	// Free each node's containers + strings
+	if response.nodes != nil && response.nodes_count > 0 {
+		nodes_count := int(response.nodes_count)
+		nodesSlice := (*[1 << 30]C.NodeStateResponse)(
+			unsafe.Pointer(response.nodes),
+		)[:nodes_count:nodes_count]
 
-		for i := 0; i < int(response.nodes_count); i++ {
-			node := &nodes[i]
+		for i := 0; i < nodes_count; i++ {
+			node := &nodesSlice[i]
 
-			// Free containers for this node
-			if node.containers != nil {
-				containers := (*[1 << 30]C.ContainerStateResponse)(unsafe.Pointer(node.containers))[:node.containers_count:node.containers_count]
+			if node.containers != nil && node.containers_count > 0 {
+				containers_count := int(node.containers_count)
+				containersSlice := (*[1 << 30]C.ContainerStateResponse)(
+					unsafe.Pointer(node.containers),
+				)[:containers_count:containers_count]
 
-				// Free each container's strings
-				for j := 0; j < int(node.containers_count); j++ {
-					if containers[j].container_id != nil {
-						C.free(unsafe.Pointer(containers[j].container_id))
+				for j := 0; j < containers_count; j++ {
+					if containersSlice[j].container_id != nil {
+						C.free(unsafe.Pointer(containersSlice[j].container_id))
 					}
 				}
+
+				C.free(unsafe.Pointer(node.containers))
 			}
 		}
+
+		C.free(unsafe.Pointer(response.nodes))
 	}
+
+	C.free(unsafe.Pointer(response))
 }
