9a3099f6e7374120f53da6b86e42c0b68c9b4138
Juan Fernández <juan@bitomia.com>
2026-01-27 17:01:05 +0100

    fix: TailFile streaming

--- common/fs.go
+++ common/fs.go
@@ -3,6 +3,8 @@ package common
 import (
 	"bufio"
 	"io"
+	"log/slog"
+	"net/http"
 	"os"
 	"time"
 )
@@ -20,17 +22,41 @@ func isTruncated(file *os.File) (bool, error) {
 }
 
 func TailFile(filepath string, w io.Writer) error {
+	slog.Debug("TailFile starting", "filepath", filepath)
 	file, err := os.Open(filepath)
 	if err != nil {
+		slog.Error("TailFile failed to open file", "filepath", filepath, "error", err)
 		return err
 	}
 	defer file.Close()
+
+	if _, err := file.Stat(); err != nil {
+		slog.Error("TailFile failed to stat file", "filepath", filepath, "error", err)
+		return err
+	}
+
 	reader := bufio.NewReader(file)
+
+	// check if the writer supports flushing
+	flusher, canFlush := w.(http.Flusher)
+	slog.Debug("TailFile writer flusher support", "canFlush", canFlush)
 
 	for {
 		line, err := reader.ReadString('\n')
+
 		if err != nil {
 			if err == io.EOF {
+				// read a partial line, write it before waiting
+				if len(line) > 0 {
+					slog.Debug("TailFile writing partial line", "length", len(line))
+					if _, err := io.WriteString(w, line); err != nil {
+						return err
+					}
+					if canFlush {
+						flusher.Flush()
+					}
+				}
+
 				time.Sleep(100 * time.Millisecond)
 
 				truncated, errTruncated := isTruncated(file)
@@ -38,17 +64,26 @@ func TailFile(filepath string, w io.Writer) error {
 					break
 				}
 				if truncated {
+					slog.Debug("TailFile detected truncation, seeking to start")
 					_, errSeekStart := file.Seek(0, io.SeekStart)
 					if errSeekStart != nil {
 						break
 					}
+					reader = bufio.NewReader(file)
 				}
 				continue
 			}
+			slog.Debug("TailFile read error", "error", err)
 			break
 		}
+
 		if _, err := io.WriteString(w, line); err != nil {
+			slog.Error("TailFile failed to write line", "error", err)
 			return err
+		}
+
+		if canFlush {
+			flusher.Flush()
 		}
 	}
 
